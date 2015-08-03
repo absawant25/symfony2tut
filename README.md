2a223dd43a61ea0d23c17b38328b9f432c642954
absawant25 <abhisawant25@gmail.com>
2015-08-03 16:06:52 +0530

    Basic symfony2

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony2tut
+===========
+
+A Symfony project created on August 3, 2015, 10:45 am.
---
+++ src/Qualys/Bundle/NewsFeedBundle/QualysNewsFeedBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Qualys\Bundle\NewsFeedBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class QualysNewsFeedBundle extends Bundle
+{
+}
